9a21356ce9e3a3cd6cd6a11afd4cc6f7a896b579
enixdark <cqshinn92@gmail.com>
2015-03-30 11:04:19 +0700

    add simple post and get http method on tornado

---
+++ HandlerPost/post_service.py
@@ -0,0 +1,33 @@
+import tornado.httpserver
+import tornado.web
+import tornado.options
+import tornado.ioloop
+import textwrap
+
+from tornado.options import define,options
+
+PORT = 8000
+define("port",default=PORT,help="the server run at %s" % PORT,type=int)
+
+class ReveserHandler(tornado.web.RequestHandler):
+	def get(self,input):
+		self.write(input[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+	def post(self):
+		text = self.get_argument('text')
+		width = self.get_argument('width',40)
+		self.write(textwrap.fill(text,width))
+
+if __name__ == "__main__":
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(
+		handlers=[
+			(r'/reverse/(\w+)',ReveserHandler),
+			(r'/wrap',WrapHandler)
+		]
+		)
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
+
