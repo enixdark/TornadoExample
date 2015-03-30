22d6e8b9eefcc4a7b1297ab8532be928b46bd276
enixdark <cqshinn92@gmail.com>
2015-03-30 11:16:45 +0700

    update

--- HandlerPost/post_service.py
+++ HandlerPost/post_service.py
@@ -19,12 +19,35 @@ class WrapHandler(tornado.web.RequestHandler):
 		width = self.get_argument('width',40)
 		self.write(textwrap.fill(text,width))
 
+class WidgetHandler(tornado.web.RequestHandler):
+	def get(self,widget_id):
+		widget = retrive_from_db(widget_id)
+		self.write(widget.serialize())
+
+	def post(self,widget_id):
+		widget = retrive_from_db(widget_id)
+		widget['foo'] = self.get_argument('foo')
+		save_to_db(widget)
+
+class FrobHandler(tornado.web.RequestHandler):
+	def head(self,frob_id):
+		frob = retrive_from_db(frob_id)
+		if frob os not None:
+			self.set_status(200)
+		else:
+			self.set_status(404)
+
+	def get(self,frob_id):
+		frob = retrive_from_db(frob_id)
+		self.write(frob.serialize())
+
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	app = tornado.web.Application(
 		handlers=[
 			(r'/reverse/(\w+)',ReveserHandler),
-			(r'/wrap',WrapHandler)
+			(r'/wrap',WrapHandler),
+			(r"/widget/(\d+)", WidgetHandler),
 		]
 		)
 	http_server = tornado.httpserver.HTTPServer(app)
